b9215b8cda3c85be4f39958b296ca854998fd6f4
André Medeiros Silva <113383742+Andremdrs77@users.noreply.github.com>
2026-08-14 15:43:26 -0300

    Create soma_syscall1.asm
    Implement addition of two numbers using syscall.

---
+++ Aulas/Aula_01/soma_syscall1.asm
@@ -0,0 +1,16 @@
+.text
+
+#soma de dois números com syscall
+
+main: addi $2, $0, 5
+      syscall
+      add $8, $0, $2
+      addi $2, $0, 5
+      syscall
+      add $9, $0, $2
+      
+      add $4, $8, $9
+      
+      addi $2, $0, 1
+      syscall 
+      
